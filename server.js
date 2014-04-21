72105b1efbf284d7940958b5dd0d13f1ad78624e
root <root@info.(none)>
2014-04-21 02:10:30 -0400

    temp solution to download the generated woff, TODO: redirect to the download right after finishing generating, as there's no storing now; create a temporary directory for one session

--- server.js
+++ server.js
@@ -24,22 +24,26 @@ app.use(logErrors);
 app.use(clientErrorHandler);
 app.use(errorHandler);
 
+app.use(express.static(__dirname+'/TTFRender/fonts'));
+
 
 http.createServer(function(req, res) {
+    if(req.url=='/download'){
+        fs.readFile('TTFRender/fonts/new_current.woff', function (err,data) {
+        res.writeHead(200);
+        res.write(data,"binary");
+        res.end();
+        })
+        return;
+    }
     if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
         // parse a file upload
         var form = new formidable.IncomingForm();
         form.uploadDir = "TTFRender/fonts/";
         form.keepExtensions = true;
         form.parse(req, function(err, fields, files) {
-            res.writeHead(200, {
-                'content-type': 'text/plain'
-            });
-            res.write('received upload:\n\n');
-            res.end(util.inspect({
-                files: files
-            }));
-      
+            
+            var demo = fields.demo;
             var file = files.upload;
             var input = files.input;
             fs.rename(file.path, 'TTFRender/fonts/current.ttf', function (err) {
@@ -66,7 +70,24 @@ http.createServer(function(req, res) {
                             // error handling & exit
                             }
                             console.log('done gulping');
-                            res.write('done gulping');
+                            fs.readFile('TTFRender/char.txt', 'utf8', function (err,data) {
+                              if (err) {
+                                return console.log(err);
+                              }
+                              res.writeHead(200, {
+                                    'content-type': 'text/html'
+                                });
+                                //res.write('received upload:\n\n');
+                                res.end('<form action="/upload" enctype="multipart/form-data" method="post">'+
+                                    '<input type="file" name="input" multiple="multiple"><br>'+
+                                    '<input type="file" name="upload" multiple="multiple"><br>'+
+                                    '<input type="submit" value="Upload">'+
+                                    '</form>'+
+                                    '<style>@font-face { font-family: newfont; src: url("/new_current.woff"); } div{font-family:newfont;}</style>'+
+                                    '<div>'+demo+'</div>'
+                                );
+                            });
+                            
 	  
                         });
                     });
@@ -87,6 +108,7 @@ http.createServer(function(req, res) {
     });
     res.end(
         '<form action="/upload" enctype="multipart/form-data" method="post">'+
+        '<input type="text" name="demo"><br>'+
         '<input type="file" name="input" multiple="multiple"><br>'+
         '<input type="file" name="upload" multiple="multiple"><br>'+
         '<input type="submit" value="Upload">'+
